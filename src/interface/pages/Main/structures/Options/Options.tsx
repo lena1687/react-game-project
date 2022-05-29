dd8fa8e58c3981f2a87eeba19353138c4678178c
lena1687 <@451062bb>
2022-05-29 16:14:14 +0600

    Work: lists, events, forms - removing superfluous old code from previously lesson

--- src/interface/pages/Main/structures/Options/Options.tsx
+++ src/interface/pages/Main/structures/Options/Options.tsx
@@ -3,36 +3,11 @@ import { Complexity } from "./Copmlexity";
 import Themes from "./Themes";
 import styles from "./Options.sass";
 
-type Option = { shouldIRender: boolean };
-class Options extends React.Component<any, Option> {
-  constructor(props: Option) {
-    super(props);
-    this.state = {
-      shouldIRender: true,
-    };
-  }
-  componentDidMount(): void {
-    setTimeout(() => {
-      this.setState({ shouldIRender: false });
-    }, 10000);
-  }
-
-  render(): JSX.Element {
-    return (
-      <div className={styles.options}>
-        <Complexity />
-        {this.state.shouldIRender && (
-          <div>
-            <span>
-              The Block Themes will be deleted in 10 seconds for Emulate
-              'componentWillUnmount'
-            </span>
-            <Themes />
-          </div>
-        )}
-      </div>
-    );
-  }
-}
-
-export default Options;
+export const Options = (): JSX.Element => {
+  return (
+    <div className={styles.options}>
+      <Complexity />
+      <Themes />
+    </div>
+  );
+};
--- src/interface/pages/Main/structures/Options/Themes/Themes.tsx
+++ src/interface/pages/Main/structures/Options/Themes/Themes.tsx
@@ -38,7 +38,6 @@ class Themes extends React.Component<any, PropsThemes> {
               isLoaded: true,
               themes: result,
             });
-            console.log("this.state", this.state);
           }
         },
         (error) => {
@@ -47,65 +46,25 @@ class Themes extends React.Component<any, PropsThemes> {
       );
   }
 
-  //componentDidMount() is called immediately after mounting (that is, inserting the component into the DOM).
-  // Actions that require DOM nodes should occur in this method. This is a good place to create network requests.
-
   componentDidMount(): void {
-    console.log("componentDidMount");
     this._isMounted = true;
     this.getThemes();
   }
-
-  //Use shouldComponentUpdate() to indicate the need for the next render based on state changes and props.
-  //By default, re-rendering occurs whenever the state changes. In most cases, you should rely on this behavior.
-  //shouldComponentUpdate() is called before rendering when it receives new props or state.
-  //The default value is true. This method is not called at the first render or when forceUpdate() is used.
-
-  shouldComponentUpdate(
-    nextProps: Readonly<any>,
-    nextState: Readonly<PropsThemes>
-  ): boolean {
-    console.log("shouldComponentUpdate");
-    return this.state.isLoaded !== nextState.isLoaded;
-  }
-
-  //An example of when to use componentDidUpdate() is when we need to call an external API on condition that
-  // the previous state and the current state have changed.
-  //The call to the API would be conditional to the state being changed. If there is no state change, no API is called.
 
   componentDidUpdate(
     nextProps: Readonly<any>,
     nextState: Readonly<PropsThemes>
   ): void {
-    console.log("componentDidUpdate");
     if (this.state.themes.length && this.state.error?.message.length) {
       this.getThemes();
     }
   }
 
-  //componentWillUnmount() is called immediately before unmounting and deleting the component.
-  //This method performs the necessary reset: canceling timers, network requests, and subscriptions created in
-  //componentDidMount().
-  //Do not use setState() in componentWillUnmount(), as the component will never be rendered again.
-
   componentWillUnmount(): boolean {
-    console.log("componentWillUnmount");
     return (this._isMounted = false);
   }
 
-  changeThemes = (): void => {
-    return this.setState({ themes: [] });
-  };
-
-  getError = (): void => {
-    return this.setState({
-      error: { message: "Stop, here the error" },
-      isLoaded: false,
-    });
-  };
-
   render(): JSX.Element {
-    console.log("render");
     const { error, isLoaded, themes } = this.state;
     if (error) {
       return <div>Error: {error.message}</div>;
@@ -119,13 +78,6 @@ class Themes extends React.Component<any, PropsThemes> {
             name="ThemesMemoryCards"
             heading="Choose the theme of cards"
           />
-          <button onClick={this.changeThemes}>
-            Emulate 'shouldComponentUpdate'-changeThemes
-          </button>
-          <br />
-          <button onClick={this.getError}>
-            Emulate 'componentDidUpdate'-getError
-          </button>
         </div>
       );
     }
