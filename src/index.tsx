145a9718064972157d8107dc6cb1b92f6ba07e4e
lena1687 <@451062bb>
2022-06-08 18:07:09 +0600

    Lesson 19: React router - fix routing-2

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { HashRouter } from "react-router-dom";
 import { App } from "./App";
 
 ReactDOM.render(
-  <HashRouter basename={window.location.pathname || ""}>
+  <HashRouter>
     <App />
   </HashRouter>,
   document.getElementById("root")
