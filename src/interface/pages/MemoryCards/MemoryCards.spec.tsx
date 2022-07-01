38dd51603fe2b7b96bf738c59dec89a3fb99b01c
lena1687 <@451062bb>
2022-07-01 17:11:43 +0600

    Lesson 21: Redux middwares and side effects - part.4

--- src/interface/pages/MemoryCards/MemoryCards.spec.tsx
+++ src/interface/pages/MemoryCards/MemoryCards.spec.tsx
@@ -8,7 +8,7 @@ describe("MemoryCards", () => {
   jest.spyOn(Object.getPrototypeOf(window.localStorage), "setItem");
   Object.setPrototypeOf(window.localStorage.setItem, jest.fn());
 
-  it("error loading page with game", async () => {
+  it("error loading  game-page", async () => {
     const history = createMemoryHistory();
     history.push("/memory-cards?userName=Andy&complexity=8&theme=animalTheme");
     const { container } = render(
@@ -16,11 +16,55 @@ describe("MemoryCards", () => {
         <Content />
       </Router>
     );
+    expect(window.localStorage.setOfImages).toBe(undefined);
     expect(container.getElementsByClassName("errorMessage").length).toBe(1);
     expect(container.getElementsByClassName("cardsWrap").length).toBe(0);
   });
 
-  it("success loading page with game", async () => {
+  it("initial success loading game-page", async () => {
+    const history = createMemoryHistory();
+    history.push("/memory-cards?userName=Andy&complexity=8&theme=animalTheme");
+    window.localStorage.setItem(
+      "setOfImages",
+      '[{"value":"animalTheme","images":["animal-1.jpg","animal-2.jpg","animal-3.jpg","animal-4.jpg","animal-5.jpg","animal-6.jpg","animal-7.jpg","animal-8.jpg"]},{"value":"superHeroesTheme","images":["hero-1.jpg","hero-2.jpg","hero-3.jpg","hero-4.jpg","hero-5.jpg","hero-6.jpg","hero-7.jpg","hero-8.jpg"]}]'
+    );
+    const { container } = render(
+      <Router location={history.location} navigator={history}>
+        <Content />
+      </Router>
+    );
+    expect(container.getElementsByClassName("errorMessage").length).toBe(0);
+    expect(container.getElementsByClassName("cardsWrap").length).toBe(1);
+    expect(screen.getByText("So, Andy")).toBeInTheDocument();
+    const cardItem = container.getElementsByClassName("cardItem");
+    expect(cardItem).toHaveLength(8);
+  });
+
+  it("success loading game-page without progress", async () => {
+    const history = createMemoryHistory();
+    history.push("/memory-cards?userName=Andy&complexity=8&theme=animalTheme");
+    window.localStorage.setItem(
+      "setOfImages",
+      '[{"value":"animalTheme","images":["animal-1.jpg","animal-2.jpg","animal-3.jpg","animal-4.jpg","animal-5.jpg","animal-6.jpg","animal-7.jpg","animal-8.jpg"]},{"value":"superHeroesTheme","images":["hero-1.jpg","hero-2.jpg","hero-3.jpg","hero-4.jpg","hero-5.jpg","hero-6.jpg","hero-7.jpg","hero-8.jpg"]}]'
+    );
+    const { container } = render(
+      <Router location={history.location} navigator={history}>
+        <Content />
+      </Router>
+    );
+    expect(window.localStorage.setOfImages).not.toBe(undefined);
+    expect(window.localStorage.progress).toBe(undefined);
+    const images = container.querySelectorAll(".cardItem");
+    const progress = window.localStorage.progress
+      ? JSON.parse(window.localStorage.progress)
+      : [];
+    images.forEach((card, index) => {
+      const pattern = progress[index] ? /^animal-([1-8]).jpg/ : /cardBack/;
+      expect(card.getAttribute("alt")).toMatch(pattern);
+    });
+  });
+
+  it("success loading game-page with progress", async () => {
     const history = createMemoryHistory();
     history.push("/memory-cards?userName=Andy&complexity=8&theme=animalTheme");
     window.localStorage.setItem(
@@ -36,11 +80,8 @@ describe("MemoryCards", () => {
         <Content />
       </Router>
     );
-    expect(container.getElementsByClassName("errorMessage").length).toBe(0);
-    expect(container.getElementsByClassName("cardsWrap").length).toBe(1);
-    expect(screen.getByText("So, Andy")).toBeInTheDocument();
-    const cardItem = container.getElementsByClassName("cardItem");
-    expect(cardItem).toHaveLength(8);
+    expect(window.localStorage.setOfImages).not.toBe(undefined);
+    expect(window.localStorage.progress).not.toBe(undefined);
     const images = container.querySelectorAll(".cardItem");
     const progress = window.localStorage.progress
       ? JSON.parse(window.localStorage.progress)
